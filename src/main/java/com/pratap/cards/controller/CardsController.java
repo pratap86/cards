b5523b1448a934d49536e54b784feef7f4994b9f
pratap86 <narayanpratap86@gmail.com>
2022-09-13 19:30:10 +0530

    modified request mapping

--- src/main/java/com/pratap/cards/controller/CardsController.java
+++ src/main/java/com/pratap/cards/controller/CardsController.java
@@ -5,15 +5,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.pratap.cards.config.CardsServiceConfig;
 import com.pratap.cards.model.Cards;
-import com.pratap.cards.model.Customer;
 import com.pratap.cards.model.Properties;
 import com.pratap.cards.repository.CardsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -28,10 +27,10 @@ public class CardsController {
     private CardsServiceConfig cardsServiceConfig;
 
     @GetMapping("/myCards")
-    public ResponseEntity<List<Cards>> getCardDetails(@RequestBody Customer customer) {
-        List<Cards> cards = cardsRepository.findByCustomerId(customer.getCustomerId());
+    public ResponseEntity<List<Cards>> getCardDetails(@RequestParam int customerId) {
+        List<Cards> cards = cardsRepository.findByCustomerId(customerId);
         if (cards != null && !cards.isEmpty())
-            return new ResponseEntity<>(cards, HttpStatus.FOUND);
+            return new ResponseEntity<>(cards, HttpStatus.OK);
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
